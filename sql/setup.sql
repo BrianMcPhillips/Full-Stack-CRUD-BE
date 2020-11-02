af4325e20c0d0bd4042bd2885002f5f510c0eada
Brian McPhillips <B.McPhillips89@gmail.com>
2020-11-02 09:06:01 -0800

    morning commit

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,12 +1,11 @@
-DROP TABLE IF EXISTS Microphones;
+DROP TABLE IF EXISTS microphones;
 
-CREATE TABLE Microphones (
+CREATE TABLE microphones (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   model TEXT NOT NULL,
   brand TEXT NOT NULL,
   pattern TEXT NOT NULl,
   microphone_type TEXT,
   price DOUBLE PRECISION,
-  summary TEXT,
-  awesome BOOLEAN
+  summary TEXT
 )
